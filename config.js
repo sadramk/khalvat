37459afe943642cb085d51e56c462755ca22d7ff
sadra mk <sadrask2005@gmail.com>
2021-12-26 23:31:25 +0330

    first commit

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    app: {
+        px: '!',
+        token: 'OTA4NjYyNTc3ODcyMTM0MTU0.YY4_-A.fYor7b5pSVynPjJubyAVInSnyWM',
+        playing: 'by sadramk ❤️'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
